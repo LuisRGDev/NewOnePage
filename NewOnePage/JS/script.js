3e0b563723533a4be8447b075ef1d7d123f35da6
Luis Alberto Rosales García <luisalbertorosalesg@gmail.com>
2026-02-16 11:13:46 -0600

    Delete NewOnePage directory

--- NewOnePage/JS/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const options = document.querySelector(".options");
-
-hamburger.addEventListener("click", () => {
-    options.classList.toggle("active");
-});
